e0c90be2bd570c8e55ba20b83f57b77a10c483c7
Hung Pham <Hung Pham@DESKTOP-ATT7B5O>
2020-07-29 15:51:24 +0700

    init template for web spa

--- web-spa/src/App.vue
+++ web-spa/src/App.vue
@@ -2,59 +2,100 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="white"
+      src="https://picsum.photos/1920/1080?random"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title>Todoist</v-toolbar-title>
     </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
 
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
-      <HelloWorld/>
+      <v-container fluid>
+        <v-row dense>
+           <v-col
+          cols="3"
+          >
+            <v-card>
+              <v-card-title>Sẽ làm</v-card-title>
+               <v-container>
+                  <v-row dense>
+                <v-col cols="12">
+                   <v-card
+                    color="#385F73"
+                    dark
+                  >
+                    <v-card-title class="headline">Unlimited music now</v-card-title>
+
+                    <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+                    </v-card>
+                </v-col>
+              </v-row>
+               </v-container>
+              
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
-    //
+    drawer: false,
   }),
 };
 </script>
---
+++ web-spa/src/common/BaseApi.js
@@ -0,0 +1,4 @@
+/* eslint-disable */
+const BASE_URL = 'https://fancy-project-api-sample.herokuapp.com/api/v1/'
+//const BASE_URL = 'http://localhost:33717/api/v1/'
+export default BASE_URL
