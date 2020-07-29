b5c95069e22080db8ee815fa6fd5dfa743977442
Hung Pham <Hung Pham@DESKTOP-ATT7B5O>
2020-07-29 23:27:26 +0700

    Create Todo App UI

--- web-spa/src/App.vue
+++ web-spa/src/App.vue
@@ -1,97 +1,50 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="white"
-      src="https://picsum.photos/1920/1080?random"
-      dark
-    >
-    <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+  <div>
+    <span class="bg">
+      <v-app>
+        <v-app-bar app color="white" src="https://picsum.photos/1920/1080?random" dark>
+          <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Todoist</v-toolbar-title>
-    </v-app-bar>
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
+          <v-toolbar-title>Todoist</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>mdi-logout</v-icon>
+          </v-btn>
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer" absolute temporary>
+          <v-list nav dense>
+            <v-list-item-group active-class="deep-white--text text--accent-4">
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-note</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Notes</v-list-item-title>
+              </v-list-item>
 
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
-
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-    <v-main>
-      <v-container fluid>
-        <v-row dense>
-           <v-col
-          cols="3"
-          >
-            <v-card>
-              <v-card-title>Sẽ làm</v-card-title>
-               <v-container>
-                  <v-row dense>
-                <v-col cols="12">
-                   <v-card
-                    color="#385F73"
-                    dark
-                  >
-                    <v-card-title class="headline">Unlimited music now</v-card-title>
-
-                    <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
-                    </v-card>
-                </v-col>
-              </v-row>
-               </v-container>
-              
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-target</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Targets</v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-navigation-drawer>
+        <v-main>
+          <Board />
+        </v-main>
+      </v-app>
+    </span>
+  </div>
 </template>
 
 <script>
-
+import Board from "./components/Board";
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
+    Board: Board,
   },
 
   data: () => ({
@@ -99,3 +52,5 @@ export default {
   }),
 };
 </script>
+<style scoped>
+</style>
---
+++ web-spa/src/components/Board.vue
@@ -0,0 +1,145 @@
+<template>
+  <v-container fluid @click="openEditNote">
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{dialogTitle}}</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field label="Title*" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-textarea name="input-7-1" filled label="Content*" auto-grow></v-textarea>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-row dense>
+      <v-col v-for="target in targets" :key="target.id" :cols="target.flex">
+        <v-hover v-slot:default="{ hover }" open-delay="200">
+          <v-card :elevation="hover ? 16 : 2"> 
+            <v-card-title v-text="target.title"></v-card-title>
+            <draggable class="dragArea list-group" :list="target.tasks" group="spec">
+            <v-container>
+              <v-row dense>
+                <draggable class="dragArea list-group" :list="target.tasks" group="spec" @change="handleChangCard($event, )">
+                  <transition-group>
+                  <v-col cols="12" v-for="task in target.tasks" :key="task.id">
+                    <div class="note-item">
+                      <v-card :color="target.color" dark class="detail-note" :id="task.id">
+                        <v-card-title class="headline note-item">{{task.taskTitle}}</v-card-title>
+                        <v-card-subtitle class="note-item" :id="task.id">{{task.content}}</v-card-subtitle>
+                      </v-card>
+                    </div>
+                  </v-col>
+                  </transition-group>
+                </draggable>
+              </v-row>
+            </v-container>
+            </draggable>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn icon class="add-note">
+                <v-icon class="add-note">mdi-plus</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-hover>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+export default {
+  name: "Board",
+  components: {
+    draggable,
+  },
+  data: () => ({
+    targets: [
+      {
+        id: 1,
+        title: "chưa làm",
+        flex: 4,
+        color: "#22472C",
+        tasks: [
+          {
+            id: 6,
+            taskTitle: "test 1",
+            content:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu semper ante. Aliquam magna diam, rhoncus non egestas ac, scelerisque a nibh. Maecenas in ipsum magna. Nunc condimentum sodales eros nec sagittis. Nulla fermentum, enim sit amet hendrerit malesuada, lorem sapien vehicula erat, non congue felis massa quis libero. Nulla euismod quam ac ex facilisis posuere. Donec ac urna viverra, dignissim nisi eget, bibendum nulla.",
+          },
+          { id: 7, taskTitle: "test 2", content: "test 2" },
+        ],
+      },
+      {
+        id: 2,
+        title: "đang làm",
+        flex: 4,
+        color: "#385F73",
+        tasks: [
+          { id: 8, taskTitle: "visual studio", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vestibulum, sapien eu lobortis malesuada, diam leo finibus diam, in elementum lacus ipsum in libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas eget mauris dui. Integer non tortor nec dolor semper dictum. Aliquam lacinia tempus iaculis. Aenean rutrum congue tempor. Quisque commodo semper ante, eget eleifend enim malesuada et. Aenean tincidunt mollis ante, at ultrices eros interdum ut. Phasellus dui sapien, pellentesque vel mi at, feugiat auctor ipsum." },
+          { id: 9, taskTitle: "jetbrains", content: "jetbrains" },
+          { id: 10, taskTitle: "eclips", content: "eclips" },
+        ],
+      },
+      {
+        id: 3,
+        title: "sẽ làm",
+        flex: 4,
+        color: "#40060B",
+        tasks: [],
+      },
+    ],
+    dialog: false,
+    dialogTitle: "",
+    noteId: ""
+  }),
+  methods: {
+    openEditNote(e) {
+      if (e.target.matches(".note-item")) {
+        this.noteId = e.target.id;
+        this.dialog = true;
+        this.dialogTitle = "Edit Note";
+      }
+      if (e.target.matches(".add-note")) {
+        this.dialog = true;
+        this.dialogTitle = "Add Note";
+      }
+    },
+    handleChangCard(e){
+      let id = 0;
+      if(Object.prototype.hasOwnProperty.call(e, "moved")){
+        id = e.moved.element.id;
+      }
+      else if(Object.prototype.hasOwnProperty.call(e, "removed"))
+      {
+        id = e.removed.element.id;
+      }
+      else if(Object.prototype.hasOwnProperty.call(e, "added")){
+        id = e.added.element.id;
+      }
+      console.log(id);
+    }
+  },
+};
+</script>
+<style scoped>
+.detail-note{
+  cursor: all-scroll;
+  height:100% !important
+}
+</style>
